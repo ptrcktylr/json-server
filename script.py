ebed4b61bc9090ed93f00cd634e44ff19cf93884
ptrcktylr <ptrcktylr@icloud.com>
2021-11-10 08:38:43 -0500

    script now adds random name from list of names, updated db file

--- script.py
+++ script.py
@@ -1,11 +1,15 @@
 import json
+import random
+
+# list of names to add to addresses
+names = ['United States Postal Service', 'Rush My Passport', 'Express Passport', 'Fastport Passport', 'Walgreens Photo', 'US Passport Office', 'ItsEasy Passport & Visa']
 
 # read old file
 with open('old_db.json', 'r') as f:
     data = json.load(f)
     # for each address add a name field
     for address_obj in data['addresses']:
-        address_obj['name'] = "United States Postal Service"
+        address_obj['name'] = random.choice(names)
     # write to new file
     with open('db.json', 'w') as new_f:
         json.dump(data, new_f, indent=4)
